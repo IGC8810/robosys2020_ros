ae7ca76a2df8e18b9c350293466d6ae193aa2d0e
IGC8810 <hash.seven.haya@gmail.com>
2021-01-08 14:30:25 +0000

    edit file

--- scripts/subscribe.py
+++ scripts/subscribe.py
@@ -3,10 +3,10 @@ import rospy
 from std_msgs.msg import Int32
 
 def cb(message):
-    rospy.loginfo(message.data * 2)
+    rospy.loginfo(message.data)
 
 if __name__ == '__main__':
     rospy.init_node('twice')
-    sub = rospy.Subscriber('countup', Int32, cb)
+    sub = rospy.Subscriber('count_up', Int32, cb)
     rospy.spin()
 
